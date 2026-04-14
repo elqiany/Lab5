122e5b31a36e07cb5af380509caca48a1ea61db7
alanpham06 <alanp@andrew.cmu.edu>
2026-04-13 22:39:11 -0400

    minor modifications to Task1 for compatibility with RISC240

--- Task1.asm
+++ Task1.asm
@@ -1,10 +1,12 @@
+; Performs multiplication on RISC240
+
     .ORG $0100
 
     ;load A into r1
     LI r4, $B010
-    LW R1, R4, $0
+    LW r1, r4, $0
 
-    ;load B into r2
+    ;load B into R2
     LI r4, $B012
     LW r2, r4, $0
 
@@ -14,7 +16,8 @@
     ;save og B sign
     MV r5, r2
     ;r5 = 8000 if neg else 0
-    ANDI r5, r5, $8000
+    LI r6, $8000
+    AND r5, r5, r6
 
     ;take abs val of B if negative
     BRZ CHECK
@@ -22,23 +25,25 @@
     NOT r2, r2
     ADDI r2, r2, $1
 
-CHECK:
+CHECK
     ;loop counter = 8
     LI r6, $0008
 
-LOOP:
+LOOP
     ;check LSB of r2
-    ANDI r7, r2, $0001
+    LI r7, $0001
+    AND r7, r2, r7
     BRZ SKIP_ADD
 
     ADD r3, r3, r1
 
-SKIP_ADD:
-    ;shift A left
-    ADD r1, r1, r1
+SKIP_ADD
 
     ;shift B right
-    SR r2, r2
+    SRLI r2, r2, $0001
+
+    ;shift A left
+    SLLI r1, r1, $0001
 
     ;decrement counter
     ADDI r6, r6, $FFFF
@@ -46,23 +51,24 @@ SKIP_ADD:
 
     BRA LOOP
 
-DONE:
+DONE
     ;if original B neg -> neg res
+    OR r5, r5, r0 
     BRZ STORE;
     NOT r3, r3
-    ADDI r3, r3, $1
+    ADDI r3, r3, $0001
 
-STORE:
+STORE
     ;store result to mem
     LI r4, $B000
-    SW r3, r4, $0
+    SW r4, r3, $0000
 
     STOP
 
 ; data
 
     .ORG $B010
-A:  .DW  $0003
-B:  .DW  $0004
+A   .DW  $007F
+B   .DW  $FF80
 
 
