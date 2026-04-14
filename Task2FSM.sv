a0e84fc86fee5ef3a6889172779f9fabdbfef44a
Elaine Qian <eqian@ece005.ece.local.cmu.edu>
2026-04-14 19:40:25 -0400

    fixed some things in fsm

--- Task2FSM.sv
+++ Task2FSM.sv
@@ -2,6 +2,7 @@
 
 //FSM for Task2 of Lab5
 //Outputs signals like enables and clear
+//but also outputs done and ZN flag
 module FSM(
     output logic prod_en,
     output logic prod_clr,
@@ -12,21 +13,26 @@ module FSM(
     output logic ct_en,
     output logic ct_clear,
     output logic ct_ld,
+    output logic prod_sel,
+    output logic a_sel,
+    output logic b_sel,
     output logic done,
-    output logic [1:0] ZN flags,
+    output logic [1:0] ZN_flags,
     input logic start,
     input logic lsbB1,
     input logic N_flag,
     input logic Z_flag,
-    input logic clock);
+    input logic clock,
+    input logic reset
+    input logic exit_loop);
 
     typedef enum logic [2:0] {
-        IDLE = 2'b000,
-        INIT = 2'b001,
-        SHIFT = 2'b010,
-        ADD = 2'b011,
-        CHECK = 2'b100,
-        FINISH  = 2'b101
+        IDLE = 3'b000,
+        INIT = 3'b001,
+        SHIFT = 3'b010,
+        ADD = 3'b011,
+        CHECK = 3'b100,
+        FINISH  = 3'b101
     } state_t;
 
     state_t currState, nextState;
@@ -45,7 +51,6 @@ module FSM(
             IDLE: begin
                 if (start) begin
                     nextState = INIT;
-
                     prod_en = 1'b1;
                     prod_sel = 1'b0;
                     a_en = 1'b1;
@@ -60,7 +65,6 @@ module FSM(
             INIT: begin
                 if (lsbB1) begin
                     nextState = ADD;
-
                     prod_en = 1'b1;
                     prod_sel = 1'b1;
                     a_en = 1'b0;
@@ -72,7 +76,6 @@ module FSM(
                 end
                 else begin
                     nextState = SHIFT;
-
                     prod_en = 1'b0;
                     prod_sel = 1'b1;
                     a_en = 1'b1;
@@ -86,7 +89,6 @@ module FSM(
 
             ADD: begin
                 nextState = SHIFT;
-
                 prod_en = 1'b0;
                 prod_sel = 1'b1;
                 a_en = 1'b1;
@@ -101,7 +103,6 @@ module FSM(
             SHIFT: begin
                 if (exit_loop) begin
                     nextState = CHECK;
-
                     prod_en = 1'b0;
                     prod_sel = 1'b1;
                     a_en = 1'b0;
@@ -110,13 +111,10 @@ module FSM(
                     b_sel = 1'b1;
                     ct_en = 1'b0;
                     ct_ld = 1'b0;
-
                 end
                 else begin
                     //check
                     nextState = INIT;
-
-                    nextState = CHECK;
                     prod_en = 1'b0;
                     prod_sel = 1'b1;
                     a_en = 1'b0;
@@ -129,18 +127,21 @@ module FSM(
             end
 
             CHECK: begin
-                if (N_flag = 1'b0 & Z_flag = 1'b0)
+                if (N_flag == 1'b0 & Z_flag == 1'b0) begin
                     nextState = FINISH;
                     done = 1'b1;
-                    ZN Flag = 2'b00;
-                if (N_flag = 1'b0 & Z_flag = 1'b1)
+                    ZN_flags = 2'b00;
+                end
+                if (N_flag == 1'b0 & Z_flag == 1'b1) begin
                     nextState = FINISH;
                     done = 1'b1;
-                    ZN Flag = 2'b10;
-                if (N_flag = 1'b1 & Z_flag = 1'b0)
+                    ZN_flags = 2'b10;
+                end
+                if (N_flag == 1'b1 & Z_flag == 1'b0) begin
                     nextState = FINISH;
                     done = 1'b1;
-                    ZN Flag = 2'b01;
+                    ZN_flags = 2'b01;
+                end
             end
 
             FINISH: begin
